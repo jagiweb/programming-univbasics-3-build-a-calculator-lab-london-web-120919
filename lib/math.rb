82e8576671f9dfe8dccc9ab7bdc0aebfc5ed0f0d
Javier Goncalves <jagi.ccs@gmail.com>
2019-11-25 23:19:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,11 +9,12 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  divides = num1/num2
+  divides
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1 * num2
 end
 
 def modulo(num1, num2)
